485327c5fae6c2a5f5ce8821aef165843aef206a
ramonabreu-data <cdevabreu@gmail.com>
2023-07-19 16:33:40 -0300

    01-ExpressõesNuméricas

---
+++ fundamentos-01/src/main/java/org/example/ExpressoesAritmeticas_01.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class ExpressoesAritmeticas_01 {
+    /*
+     operadores:
+
+      + Adição
+      - subitração
+
+      * Multiplicação
+
+      / Divisão
+
+      % Resto da Divisão ou Mod ou Módulo
+
+
+      Os operadores Tem precedência Superior:  * , / , %
+
+
+       Os operadores Tem precedência Inferior:  + , -
+
+
+     */
+
+
+
+
+}
