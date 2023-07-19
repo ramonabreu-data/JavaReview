f6795edf8c08b9275895010add05ffdda78535d0
ramonabreu-data <cdevabreu@gmail.com>
2023-07-19 16:34:18 -0300

    02-Variáveis e Tipos Básicos

---
+++ fundamentos-01/src/main/java/org/example/VariaveisEtiposBasicos_02.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class VariaveisEtiposBasicos_02 {
+
+    //Variáveis vai armazenar um conjunto de dados, um porção de memória RAM
+
+    //Como declarar?
+    // <Tipo> < Nome_da_Variável>  = <Valor_da_Variável> ;
+
+    /*
+      --- Dicas para declarar suas Variáveis ---
+
+      1- Não pode começar cum número : use letra ou _
+      2 - Não pode ter espaço em branco
+      3- Não pode usar acentos ou ~
+
+      -- Dica Bônus--
+
+      usePadraoCamelCase para nomear as suas variáveis
+
+    */
+
+
+    int minhaIdade = 28;
+
+    double minhaAltura = 1.70;
+
+    char sexoMeu = 'M';
+    String meuNome = "Ramon Abreu";
+
+    boolean euBrasileiro = true;
+
+
+
+
+}
